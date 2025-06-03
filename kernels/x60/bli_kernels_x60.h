c7a9fe2e1558f1946444e2f7536815929a4571a7
Stepan Nassyr <s.nassyr@fz-juelich.de>
2025-06-04 01:23:24 +0200

    x60: fp32 axpyv,copyv,packm and gemm (no trsm yet)

--- kernels/x60/bli_kernels_x60.h
+++ kernels/x60/bli_kernels_x60.h
@@ -34,6 +34,7 @@
 
 //GEMM_UKR_PROT( float,    s, gemm_x60_2vx14 )
 GEMM_UKR_PROT( double, d, gemm_x60_2vx14_2u )
+GEMM_UKR_PROT( float, s, gemm_x60_2vx14_2u )
 //GEMM_UKR_PROT( scomplex, c, gemm_x60_2vx14 )
 //GEMM_UKR_PROT( dcomplex, z, gemm_x60_2vx14 )
 
@@ -42,8 +43,11 @@ GEMMTRSM_UKR_PROT(double, d, gemmtrsm_u_x60_2vx14)
 
 
 COPYV_KER_PROT( double, d, copyv_x60)
+COPYV_KER_PROT( float, s, copyv_x60)
 AXPYV_KER_PROT( double, d, axpyv_x60)
+AXPYV_KER_PROT( float, s, axpyv_x60)
 
 PACKM_KER_PROT( double, d, packm_x60_2vx14 )
 PACKM_KER_PROT( double, d, packm_x60 )
+PACKM_KER_PROT( float, s, packm_x60 )
 
