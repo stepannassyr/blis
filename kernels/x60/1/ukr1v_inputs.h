f81b5cf00a300593494d57c5e6f7078d5a50f92c
Stepan Nassyr <s.nassyr@fz-juelich.de>
2025-06-19 14:59:28 +0200

    x60: fix ukr1_8v.h

--- kernels/x60/1/ukr1v_inputs.h
+++ kernels/x60/1/ukr1v_inputs.h
@@ -1,11 +1,13 @@
 #include <stdint.h>
 
-#define UKR1_4VM2
+//#define UKR1_4VM2
 
 #if defined(UKR1_4VM2)
 #define UKRINCLUDE "ukr1_4vm2.h"
+#define LMUL "2"
 #else
 #define UKRINCLUDE "ukr1_8v.h"
+#define LMUL "1"
 #endif
 
 #define TAILPREPARE TAILPREPARE_VREST
